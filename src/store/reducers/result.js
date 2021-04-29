a7f9b2f8c06c96de1945536f876a62661c9dd7af
tuanna2122 <tuanna2122@gmail.com>
2021-04-30 00:30:35 +0700

    redux advance continue

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,28 +1,26 @@
-import * as actionTypes from '../actions/actions';
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultId);
+    return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
+            return updateObject(state, {
                 results: state.results.concat({
                     id: new Date(),
                     value: action.result
                 })
-            };
+            });
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultId);
-            return {
-                ...state,
-                results: updatedArray
-            };
+            return deleteResult(state, action);
     }
     return state;
 }
